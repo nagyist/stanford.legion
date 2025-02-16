3cd9ec31d709e0e227a7098480dfc6f7d21cc19c
Mike <mebauer@cs.stanford.edu>
2025-02-15 22:15:39 -0800

    legion: updates to c and f bindings

--- runtime/legion/bindings/c_bindings_util.h
+++ runtime/legion/bindings/c_bindings_util.h
@@ -1,4 +1,4 @@
-/* Copyright 2024 Stanford University
+/* Copyright 2025 Stanford University
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@
  */
 
 #include "legion.h"
+#include "legion/bindings/c_bindings.h"
 #include "mappers/mapping_utilities.h"
 
 #include <stdlib.h>
