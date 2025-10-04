b03e46a080f3733d26fd5060c5fd2e418ddd1f4f
Mike <mebauer@cs.stanford.edu>
2025-10-03 19:22:13 -0700

    tools: improve pattern matching for color determination for processor kinds

--- tools/legion_prof_rs/src/backend/data_source.rs
+++ tools/legion_prof_rs/src/backend/data_source.rs
@@ -255,29 +255,16 @@ impl StateDataSource {
                 kind_index += 1;
 
                 let color = match (kind, device) {
-                    (ProcKind::GPU, None) => Color::ORANGERED, // No device work
-                    (ProcKind::GPU, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
-                    (ProcKind::GPU, Some(DeviceKind::Host)) => Color::ORANGERED,
-                    (ProcKind::CPU, None) => Color::STEELBLUE,
-                    (ProcKind::CPU, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
-                    (ProcKind::CPU, Some(DeviceKind::Host)) => Color::STEELBLUE,
-                    (ProcKind::Utility, None) => Color::CRIMSON,
-                    (ProcKind::IO, None) => Color::ORANGERED,
-                    (ProcKind::IO, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
-                    (ProcKind::IO, Some(DeviceKind::Host)) => Color::ORANGERED,
-                    (ProcKind::ProcGroup, None) => Color::ORANGERED,
-                    (ProcKind::ProcGroup, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
-                    (ProcKind::ProcGroup, Some(DeviceKind::Host)) => Color::ORANGERED,
-                    (ProcKind::ProcSet, None) => Color::ORANGERED,
-                    (ProcKind::ProcSet, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
-                    (ProcKind::ProcSet, Some(DeviceKind::Host)) => Color::ORANGERED,
-                    (ProcKind::OpenMP, None) => Color::ORANGERED,
-                    (ProcKind::OpenMP, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
-                    (ProcKind::OpenMP, Some(DeviceKind::Host)) => Color::ORANGERED,
-                    (ProcKind::Python, None) => Color::DARKGOLDENROD,
-                    (ProcKind::Python, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
-                    (ProcKind::Python, Some(DeviceKind::Host)) => Color::DARKGOLDENROD,
-                    _ => unreachable!(),
+                    // All Device timelines get the same value
+                    (_, Some(DeviceKind::Device)) => Color::OLIVEDRAB,
+                    (ProcKind::GPU, _) => Color::ORANGERED,
+                    (ProcKind::CPU, _) => Color::STEELBLUE,
+                    (ProcKind::Utility, _) => Color::CRIMSON,
+                    (ProcKind::IO, _) => Color::ORANGERED,
+                    (ProcKind::ProcGroup, _) => Color::ORANGERED,
+                    (ProcKind::ProcSet, _) => Color::ORANGERED,
+                    (ProcKind::OpenMP, _) => Color::ORANGERED,
+                    (ProcKind::Python, _) => Color::DARKGOLDENROD,
                 };
                 let color: Color32 = color.into();
 
